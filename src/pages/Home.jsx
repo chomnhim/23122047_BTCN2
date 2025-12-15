50b0f5d9b57bca8ee86b46b7636a4cfc15368928
23122047 <xuanquang.tn94@gmail.com>
2025-12-15 12:13:37 +0700

    danh sách movie được đánh giá cao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,18 @@ import MovieRow from "../components/MovieRow";
 export default function Home() {
   return (
     <div className="home-page" style={{ paddingBottom: '40px' }}>
-      
       <HeroSlider />
 
       <MovieRow 
         title="Most Popular" 
         type="popular" 
       />
+
+      <MovieRow 
+        title="Top Rating" 
+        type="top_rated" 
+      />
+
     </div>
   );
 }
